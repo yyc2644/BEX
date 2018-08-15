ebe94b2d114982ef0a39cead0a785e28a4ee5494
yyc2644 <yichengace@hotmail.com>
2018-08-15 10:16:32 +0800

    0815_1
    修改了一下交易机器人

--- robot.py
+++ robot.py
@@ -1,6 +1,5 @@
 #尝试跑机器人
 #目前平台的发布交易，可以通过接口方式实现，用python发送参数来模拟挂单行为
-import urllib
 import requests
 import json
 RequsetUrl  = "http://192.168.1.248:9079/api/transaction/api/v1/user/entrust"
@@ -10,16 +9,32 @@ RequestHeaders = {
     "currLanguage":"zh",
     "Content-Type":"application/json;charset=UTF-8"
 }
-RequestBody = {"entrustPrice":"1","entrustCount":"1","marketId":31,"type":"1","dealPassword":"qwe123"}
+ByeRequestBody = {"entrustCount":"1","entrustPrice":"1","marketId":31,"":"1","dealPassword":"qwe123"}
+ShellRequestBody = {"entrustPrice":"1","entrustCount":"1","marketId":31,"type":"1","dealPassword":"qwe123"}
 
-request = requests.session()
+def entrust(entrustPrice,entrustCount,marketId,Type,dealPassword):
+    #委托价格，委托数量，交易对名称，买/卖，确认密码
+    {
+        "entrustCount":entrustCount,
+        "entrustPrice":entrustPrice,
+        "marketId":marketId,
+        "type":Type,
+        "dealPassword":dealPassword
+    }
 
-req = requests.post(RequsetUrl,data=json.dumps(RequestBody),headers = RequestHeaders)
-# header_dict = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko'}
-print(req.text)
 
-# REQ = requests('psot',RequsetUrl，data = RequestBody,)
 
-#     return request('post', url, data=data, json=json, **kwargs)
+# ByeReq = requests.post(
+#     RequsetUrl,
+#     data=json.dumps(ByeRequestBody),
+#     headers = RequestHeaders
+#     )
+ShellReq = requests.post(
+    RequsetUrl,
+    data=json.dumps(entrust(1,1,31,1,"qwe123")),
+    headers = RequestHeaders
+    )
+print(ShellReq.text)
 
- 
+
+
